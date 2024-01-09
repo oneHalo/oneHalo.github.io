3ccf18766cee2e64d084801479ee9c3243316aab
Wahid Hoji Aminullah <wahidwahidy77@gmail.com>
2024-01-09 14:01:38 -0700

    final-commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
 //import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './component/Home';
+// import Home from './component/Home';
 
-import Contact from './component/Contact';
+// import Contact from './component/Contact';
 
 function App() {
   
 
   return (
       <div>
-        {Home}
+       
       </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import Home from "./component/Home";
+import Home from "./Home";
 import reportWebVitals from './reportWebVitals';
-import Contact from './component/Contact';
+import Contact from './Contact';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
